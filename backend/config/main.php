b229dc4bf7696db1aeb3a1fd8627d6b707f908b7
azimjon10 <azimjon1998@mail.com>
2024-02-15 18:27:31 +0500

    Integrate SB-Admin 2 in Project

--- backend/config/main.php
+++ backend/config/main.php
@@ -37,6 +37,7 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+
 //        'assetManager' =>[
 //            'appendTimestamp' => true,
 //            'bundle' =>[
